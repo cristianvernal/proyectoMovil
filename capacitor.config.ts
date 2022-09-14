780eb333380a65aec632f2f1f2d69df1260918dd
cristianvernal <cr.vernal@duocuc.cl>
2022-09-14 15:12:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoMovil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
