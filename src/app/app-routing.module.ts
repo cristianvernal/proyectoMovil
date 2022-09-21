dbf9aa7c3a9d576889bc015c4926586cf8242b3b
Eve91 <evelynbarreraguevara@gmail.com>
2022-09-21 12:14:01 -0300

    Restablecer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
   },  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'restablecer',
+    loadChildren: () => import('./restablecer/restablecer.module').then( m => m.RestablecerPageModule)
   }
 
 ];
