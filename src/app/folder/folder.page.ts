e0d699592a3ffd28f7783bc141baf81aaa095f70
cristianvernal <cr.vernal@duocuc.cl>
2022-09-24 11:38:44 -0300

    bla

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -9,8 +10,9 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
+ 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
